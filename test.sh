2aa3c319056a646035346da539d3b2b702e7cc84
Koni Marti <koni.marti@gmail.com>
2026-01-23 23:57:32 +0100

    puff: add inflate to puff

--- test.sh
+++ test.sh
@@ -6,9 +6,10 @@ ffup="./build/ffup"
 ## Compress some files in the current source directory.
 for file in $(find src -type f)
 do
-	$ffup -dxf $file | xxd -r -p | puff -w | diff -s $file -
+	# $ffup -2wf $file | xxd -r -p | puff -w | diff -s $file -
+	$ffup -2wf $file | xxd -r -p | $ffup -w | diff -s $file -
 	echo ""
 done
 
 ## Compress large chunks of random data.
-dd if=/dev/random bs=1M count=32 | $ffup -dx | xxd -r -p | puff
+dd if=/dev/random bs=1M count=32 | $ffup -2w | xxd -r -p | puff
