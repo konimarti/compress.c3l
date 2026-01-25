4df440b3a1b39b998440dd6e3dc1ad7d30c582b6
Koni Marti <koni.marti@gmail.com>
2026-01-25 09:02:25 +0100

    decompressor: fix decode_symble (we need full array in code length encoding

--- test.sh
+++ test.sh
@@ -2,14 +2,14 @@
 # set -x
 c3c -O3 build ffup
 ffup="./build/ffup"
+puff="./build/ffup" # or just "puff"
 
 ## Compress some files in the current source directory.
 for file in $(find src -type f)
 do
-	# $ffup -2wf $file | xxd -r -p | puff -w | diff -s $file -
-	$ffup -2wf $file | xxd -r -p | $ffup -w | diff -s $file -
+	$ffup -2wf $file | xxd -r -p | $puff -w | diff -s $file -
 	echo ""
 done
 
 ## Compress large chunks of random data.
-dd if=/dev/random bs=1M count=32 | $ffup -2w | xxd -r -p | puff
+dd if=/dev/random bs=1M count=32 | $ffup -2w | xxd -r -p | $puff
